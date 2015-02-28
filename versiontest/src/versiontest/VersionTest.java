307f0674ad375ffdbbe39dda6f724d856091622e
A0135916B <A0135916@u.nus.edu>
2015-02-28 15:09:29 +0800

    New class added

---
+++ versiontest/src/versiontest/VersionTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package versiontest;
+
+/**
+ * @author A0135916B
+ *
+ */
+public class VersionTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Vesrion test using GIT");
+	}
+
+}
