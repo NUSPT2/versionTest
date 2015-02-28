3e8f9d713baa8c56aa0b6177907f3673972641b5
A0135916B <A0135916@u.nus.edu>
2015-02-28 15:20:02 +0800

    pushing to the server

---
+++ versiontest3/src/versiontest2/VersionTest3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package versiontest2;
+
+/**
+ * @author A0135916B
+ *
+ */
+public class VersionTest3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Vesrion test using GIT3");
+	}
+
+}
